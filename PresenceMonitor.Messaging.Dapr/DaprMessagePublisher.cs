b59d6220de769d35c4480f3b7a3adbb6f81bd2f7
Michael Kargl <michael.kargl@zuehlke.com>
2023-06-10 10:14:01 +0200

    feat: publishes events using DaprClient
    The DaprClient uses GRPC for its communication => https://github.com/dapr/dotnet-sdk/issues/809
    So we have to configure Dapr to use the GRPC port when publishing the message
    
    closes #15
    closes #16
    
    - [#15](https://gitlab.com/ohsnaparts/osasoftworks/zube/presence-monitor/-/issues/15)
    - [#16](https://gitlab.com/ohsnaparts/osasoftworks/zube/presence-monitor/-/issues/16)

--- PresenceMonitor.Messaging.Dapr/DaprMessagePublisher.cs
+++ PresenceMonitor.Messaging.Dapr/DaprMessagePublisher.cs
@@ -1,54 +1,24 @@
-using System.Net.Http.Json;
-using System.Web;
 using Configuration;
+using Dapr.Client;
 using Microsoft.Extensions.Options;
-using static System.String;
 
 public class DaprMessagePublisher : IMessagePublisher
 {
-    private readonly HttpClient _httpClient;
+    private readonly DaprClient _daprClient;
     private readonly IOptions<DaprPublisherOptions> _options;
 
-    public DaprMessagePublisher(HttpClient httpClient, IOptions<DaprPublisherOptions> options)
+    public DaprMessagePublisher(DaprClient daprClient, IOptions<DaprPublisherOptions> options)
     {
-        this._httpClient = httpClient;
+        this._daprClient = daprClient;
         this._options = options;
     }
 
     private DaprPublisherOptions DaprOptions => this._options.Value;
 
-    public async Task PublishAsync(object message, CancellationToken cancellationToken = default)
-    {
-        var response = await this.PublishDaprMessage(
-            message,
-            this.DaprOptions.DaprEndpoint!,
+    public Task PublishAsync(object message, CancellationToken cancellationToken = default) =>
+        this._daprClient.PublishEventAsync(
             this.DaprOptions.PubSubName,
             this.DaprOptions.Topic,
-            cancellationToken);
-
-        response.EnsureSuccessStatusCode();
-    }
-
-    private Task<HttpResponseMessage> PublishDaprMessage(
-        object message,
-        string daprEndpoint,
-        string pubSubName,
-        string pubSubTopic,
-        CancellationToken cancellationToken
-    )
-    {
-        var queries = HttpUtility.ParseQueryString(Empty);
-        queries.Add("metadata.rawPayload", true.ToString());
-
-        var uriBuilder = new UriBuilder(daprEndpoint)
-        {
-            Path = $"/v1.0/publish/{pubSubName}/{pubSubTopic}",
-            Query = queries.ToString()
-        };
-
-        return this._httpClient.PostAsJsonAsync(
-            uriBuilder.Uri,
             message,
             cancellationToken);
-    }
 }
